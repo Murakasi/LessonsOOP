c65049696411b18ddea0239279d09336c07d5566
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-11-21 20:34:18 +0100

    Tray work with TimeSpan

--- Lesson2/Helpers/TimeTracker.cs
+++ Lesson2/Helpers/TimeTracker.cs
@@ -6,5 +6,7 @@ namespace Lesson2.Helpers
 {
     internal class TimeTracker
     {
+        TimeSpan Start = new TimeSpan(0, 15, 0);
+        TimeSpan Finished = new TimeSpan(0, 16, 0);
     }
 }
--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TimeSpan t1 = new TimeSpan(0, 12, 00);
+            TimeSpan t2 = new TimeSpan(0, 10, 30);
+
+            TimeSpan result = t1 - t2;
+
+            Console.WriteLine(result);
+            string text = "";
+            string fileName = "text.txt";
+            if (File.Exists(fileName))
+            {
+                text = File.ReadAllText(fileName);
+            }
+
+
+            string[] words = text.Trim().Split(" ");
+            Console.WriteLine(words.Length);
+            string t = "\nHello Ruslan";
+
+            File.AppendAllText(fileName, t);
+
+
         }
     }
 }
